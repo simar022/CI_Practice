79c5cfa60d60075e0304f8f8b68e534e5dd32af7
Simar <simar022netsmartz@gmail.com>
2026-02-04 18:11:39 +0530

    Rewriting files

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify(message="Flask is running in CI/CD!")
+    return jsonify(message="Flask is running CI/CD!")
 
 if __name__ == "__main__":
+    # The server only runs if this file is executed directly
     app.run(host='0.0.0.0', port=5000)
